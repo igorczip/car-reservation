8e0521f4e63cd935ed01b855e7817b6f9b0cc491
igorczip <ippython@seznam.cz>
2026-02-09 16:10:40 +0100

    Cleanup: ignore local dumps/exports and remove backups from repo

--- env_info.py
+++
@@ -1,7 +0,0 @@
-import sys
-import platform
-
-print("Python verze:", sys.version)
-print("Executable:", sys.executable)
-print("Platform:", platform.platform())
-print("Architektura:", platform.architecture())
